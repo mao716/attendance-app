d321c30074239648c4293526246ffd731d48fc77
hamaguchi mao <kupokupo44@gmail.com>
2025-12-03 10:35:10 +0900

    feat: 会員登録機能を実装（UI・バリデーション・登録処理・遷移まで完了）

---
+++ src/app/Http/Controllers/Auth/AuthRegisterController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterRequest;
+use Laravel\Fortify\Contracts\CreatesNewUsers;
+use Illuminate\Http\RedirectResponse;
+
+class AuthRegisterController extends Controller
+{
+	public function showRegisterForm()
+	{
+		return view('auth.register');
+	}
+
+	public function store(RegisterRequest $request, CreatesNewUsers $creator): RedirectResponse
+	{
+		$user = $creator->create($request->validated());
+
+		auth()->login($user);
+
+		return redirect('/attendance');
+	}
+}
